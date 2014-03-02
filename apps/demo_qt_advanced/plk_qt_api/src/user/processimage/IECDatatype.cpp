d03913b78e66e13d94bfecf4119c6f2a0ab3d425
Ramakrishnan P <ramakrishanan_p@kalycito.com>
2014-03-02 22:05:40 +0530

    Moved definitions to CPP files.
     - No static definitions to be placed in headers so moved definitions to .cpp files.

---
+++ apps/demo_qt_advanced/plk_qt_api/src/user/processimage/IECDatatype.cpp
@@ -0,0 +1,138 @@
+/**
+********************************************************************************
+\file   IECDataType.cpp
+
+\brief  Provides the methods to access the Datatypes derived from the IEC standards.
+
+\copyright (c) 2014, Kalycito Infotech Private Limited
+					 All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of the copyright holders nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/*******************************************************************************
+* INCLUDES
+*******************************************************************************/
+#include "user/processimage/IECDataType.h"
+#include "common/Utility.h"
+
+/*******************************************************************************
+* Public functions
+*******************************************************************************/
+IECDataType GetIECDatatype(const std::string& iecDataTypeStr)
+{
+	IECDataType iecDataType = UNDEFINED;
+
+	if (CompareCaseInsensitive("BOOL", iecDataTypeStr)
+		|| CompareCaseInsensitive("Boolean", iecDataTypeStr)
+		|| CompareCaseInsensitive("BITSTRING", iecDataTypeStr))
+	{
+		iecDataType = IEC_BOOL;
+	}
+	else if (CompareCaseInsensitive("BYTE", iecDataTypeStr))
+	{
+		iecDataType = IEC_BYTE;
+	}
+	else if (CompareCaseInsensitive("CHAR", iecDataTypeStr))
+	{
+		iecDataType = IEC_CHAR;
+	}
+	else if (CompareCaseInsensitive("WORD", iecDataTypeStr))
+	{
+		iecDataType = IEC_WORD;
+	}
+	else if (CompareCaseInsensitive("DWORD", iecDataTypeStr))
+	{
+		iecDataType = IEC_DWORD;
+	}
+	else if (CompareCaseInsensitive("LWORD", iecDataTypeStr))
+	{
+		iecDataType = IEC_LWORD;
+	}
+	else if (CompareCaseInsensitive("SINT", iecDataTypeStr)
+			|| CompareCaseInsensitive("Integer8", iecDataTypeStr))
+	{
+		iecDataType = IEC_SINT;
+	}
+	else if (CompareCaseInsensitive("INT", iecDataTypeStr)
+			|| CompareCaseInsensitive("Integer16", iecDataTypeStr))
+	{
+		iecDataType = IEC_INT;
+	}
+	else if (CompareCaseInsensitive("DINT", iecDataTypeStr)
+			|| CompareCaseInsensitive("Integer32", iecDataTypeStr))
+	{
+		iecDataType = IEC_DINT;
+	}
+	else if (CompareCaseInsensitive("LINT", iecDataTypeStr)
+			|| CompareCaseInsensitive("Integer64", iecDataTypeStr))
+	{
+		iecDataType = IEC_LINT;
+	}
+	else if (CompareCaseInsensitive("USINT", iecDataTypeStr)
+			|| CompareCaseInsensitive("Unsigned8", iecDataTypeStr))
+	{
+		iecDataType = IEC_USINT;
+	}
+	else if (CompareCaseInsensitive("UINT", iecDataTypeStr)
+			|| CompareCaseInsensitive("Unsigned16", iecDataTypeStr))
+	{
+		iecDataType = IEC_UINT;
+	}
+	else if (CompareCaseInsensitive("UDINT", iecDataTypeStr)
+			|| CompareCaseInsensitive("Unsigned32", iecDataTypeStr))
+	{
+		iecDataType = IEC_UDINT;
+	}
+	else if (CompareCaseInsensitive("ULINT", iecDataTypeStr)
+			|| CompareCaseInsensitive("Unsigned64", iecDataTypeStr))
+	{
+		iecDataType = IEC_ULINT;
+	}
+	else if (CompareCaseInsensitive("REAL", iecDataTypeStr)
+			|| CompareCaseInsensitive("REAL32", iecDataTypeStr))
+	{
+		iecDataType = IEC_REAL;
+	}
+	else if (CompareCaseInsensitive("LREAL", iecDataTypeStr)
+			|| CompareCaseInsensitive("REAL64", iecDataTypeStr))
+	{
+		iecDataType = IEC_LREAL;
+	}
+	else if (CompareCaseInsensitive("STRING", iecDataTypeStr)
+			|| CompareCaseInsensitive("VISIBLE_STRING", iecDataTypeStr))
+	{
+		iecDataType = IEC_STRING;
+	}
+	else if (CompareCaseInsensitive("WSTRING", iecDataTypeStr))
+	{
+		iecDataType = IEC_WSTRING;
+	}
+	else
+	{
+		// UnHandled DataType;
+	}
+
+	return iecDataType;
+
+} // GetIecDataType
