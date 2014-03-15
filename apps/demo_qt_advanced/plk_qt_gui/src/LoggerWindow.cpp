1daa9735f8182e184c3a77c5cfb004929a5a2434
Ramakrishnan P <ramakrishanan_p@kalycito.com>
2014-03-15 14:55:19 +0530

    [FEATURE] Integrate stack logging into the GUI
    Signed-off-by: Ramakrishnan P <ramakrishanan_p@kalycito.com>

--- apps/demo_qt_advanced/plk_qt_gui/src/LoggerWindow.cpp
+++ apps/demo_qt_advanced/plk_qt_gui/src/LoggerWindow.cpp
@@ -1,12 +1,18 @@
 #include "LoggerWindow.h"
 
+#include "api/OplkQtApi.h"
+
 LoggerWindow::LoggerWindow(QWidget *parent) :
 	QDockWidget(parent)
 {
 	this->ui.setupUi(this);
+	OplkQtApi::RegisterEventLogger(*this, this->metaObject()->method(
+			this->metaObject()->indexOfMethod(QMetaObject::normalizedSignature(
+				"HandlePrintLog(const QString&)").constData())));
+
 }
 
-//LoggerWindow::HandlePrintLog(const QString&)
-//{
-
-//}
+void LoggerWindow::HandlePrintLog(const QString& str)
+{
+	this->ui.txtBxConsoleLog->appendPlainText(str);
+}
