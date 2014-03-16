ce28fbc470c13befb8dbe18410edd77b22b06838
Ramakrishnan P <ramakrishanan_p@kalycito.com>
2014-03-16 16:20:19 +0530

    Send NMT commands from GUI
     - Integrate execute NMT command API
    
    Signed-off-by: Ramakrishnan P <ramakrishanan_p@kalycito.com>

--- apps/demo_qt_advanced/plk_qt_gui/src/NmtCommandsDock.cpp
+++ apps/demo_qt_advanced/plk_qt_gui/src/NmtCommandsDock.cpp
@@ -1,5 +1,9 @@
 #include "NmtCommandsDock.h"
 
+#include <QMessageBox>
+#include "api/OplkQtApi.h"
+#include "oplk/debugstr.h"
+
 NmtCommandsDock::NmtCommandsDock(QWidget *parent) :
 	QDockWidget(parent)
 {
@@ -8,62 +12,69 @@ NmtCommandsDock::NmtCommandsDock(QWidget *parent) :
 
 void NmtCommandsDock::on_sendNmtBtn_clicked()
 {
-	// const int nodeid = this->ui.nodeId->text().toInt();
-	// nmtCommand;
-	// Execute NMT command (nodeid, nmtCommand);
+//	bool ok = false;
+//	const int nodeid = this->ui.nodeId->text().toInt(&ok, 16);
+//	if (!ok) {
+//		//NodeId hex failed, handle error here
+//		QMessageBox::critical(this, "Execute NmtCommand failed",
+//							 this->ui.nodeId->text(),
+//							 QMessageBox::Close);
+//	}
+	
+	//Send NMT command is not working for MN. 
+	// TODO in API library.
+	const int nodeid = this->ui.nodeId->text().toInt(0, 16);
+	qDebug("%d", nodeid);
+	tOplkError oplkRet = OplkQtApi::ExecuteNmtCommand(nodeid, this->nmtCommand);
+	if (oplkRet != kErrorOk)
+	{
+		QMessageBox::critical(this, "Execute NmtCommand failed",
+							 QString("Execute NmtCommand falied with error: %1 ")
+							  .arg(debugstr_getRetValStr(oplkRet)),
+							 QMessageBox::Close);
+		qDebug("Execute NmtCommand %x", oplkRet);
+		return;
+	}
 }
 
 void NmtCommandsDock::on_nmtCommand_currentIndexChanged(int index)
 {
 	/* It can be also done by parsing the string and getting the 0xXX
 	 * code to pass for the NMT command enum
-	 * /
+	 */
 
-	/* From UI list.
-	"InvalidService(0xFF)"
-	"StartNode(0x21)"
-	"StopNode(0x22)"
-	"EnterPreOperational2(0x23)"
-	"EnableReadyToOperate(0x24)"
-	"ResetNode(0x28)"
-	"ResetCommunication(0x29)"
-	"ResetConfiguration(0x2A)"
-	"SwReset(0x2B)"
-	*/
-/*
 	switch (index)
 	{
 		case 0:
-			nmtCommand = kNmtCmdInvalidService;
+			this->nmtCommand = kNmtCmdInvalidService;
 			break;
 		case 1:
-			nmtCommand = kNmtCmdStartNode;
+			this->nmtCommand = kNmtCmdStartNode;
 			break;
 		case 2:
-			nmtCommand = kNmtCmdStopNode;
+			this->nmtCommand = kNmtCmdStopNode;
 			break;
 		case 3:
-			nmtCommand = kNmtCmdEnterPreOperational2;
+			this->nmtCommand = kNmtCmdEnterPreOperational2;
 			break;
 		case 4:
-			nmtCommand = kNmtCmdEnableReadyToOperate;
+			this->nmtCommand = kNmtCmdEnableReadyToOperate;
 			break;
 		case 5:
-			nmtCommand = kNmtCmdResetNode;
+			this->nmtCommand = kNmtCmdResetNode;
 			break;
 		case 6:
-			nmtCommand = kNmtCmdResetCommunication;
+			this->nmtCommand = kNmtCmdResetCommunication;
 			break;
 		case 7:
-			nmtCommand = kNmtCmdResetConfiguration;
+			this->nmtCommand = kNmtCmdResetConfiguration;
 			break;
 		case 8:
-			nmtCommand = kNmtCmdSwReset;
+			this->nmtCommand = kNmtCmdSwReset;
 			break;
 		default:
-			nmtCommand = kNmtCmdInvalidService;
+			this->nmtCommand = kNmtCmdInvalidService;
 			break;
 	}
-*/
 }
 
