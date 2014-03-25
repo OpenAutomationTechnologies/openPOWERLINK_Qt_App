6bca42783ca000fb8d18ce32baa8ed132455b625
Ramakrishnan P <ramakrishanan_p@kalycito.com>
2014-03-25 10:28:49 +0530

    [TASK] Add license and section headers
    Signed-off-by: Ramakrishnan P <ramakrishanan_p@kalycito.com>

--- apps/demo_qt_advanced/plk_qt_gui/include/ChannelUi.h
+++ apps/demo_qt_advanced/plk_qt_gui/include/ChannelUi.h
@@ -1,41 +1,134 @@
+/**
+********************************************************************************
+\file   ChannelUi.h
+
+\brief
+
+\author Ramakrishnan Periyakaruppan
+
+\copyright (c) 2014, Kalycito Infotech Private Limited
+					 All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of the copyright holders nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
 #ifndef _UI_CHANNEL_H_
 #define _UI_CHANNEL_H_
+
+/*******************************************************************************
+* INCLUDES
+*******************************************************************************/
 
 #include "ui_ChannelUi.h"
 #include "user/processimage/Channel.h"
 #include "user/processimage/ProcessImageIn.h"
 #include "user/processimage/ProcessImageOut.h"
 
+/**
+ * @brief The ChannelUi class
+ */
 class ChannelUi : public QWidget
 {
 	Q_OBJECT
 
 public:
-	// bool has to be Pi direction
+	/**
+	 * @brief ChannelUi
+	 * @param channel
+	 * @param parent
+	 */
 	explicit ChannelUi(Channel channel, QWidget *parent = 0);
 
+	/*
+	 *
+	 */
 	~ChannelUi();
 
+	/**
+	 * @brief UpdateSelectCheckBox
+	 * @param forceState
+	 */
 	void UpdateSelectCheckBox(Qt::CheckState forceState);
 
+	/**
+	 * @brief GetSelectCheckBoxState
+	 * @return
+	 */
 	Qt::CheckState GetSelectCheckBoxState();
 
+	/**
+	 * @brief SetCurrentValue
+	 * @param setStr
+	 */
 	void SetCurrentValue(QString setStr);
 
+	/**
+	 * @brief GetCurrentValue
+	 * @return
+	 */
 	QString GetCurrentValue();
 
+	/**
+	 * @brief GetForceValue
+	 * @return
+	 */
 	QString GetForceValue();
 
+	/**
+	 * @brief UpdateForceCheckBox
+	 * @param forceState
+	 */
 	void UpdateForceCheckBox(Qt::CheckState forceState);
 
+	/**
+	 * @brief GetForceCheckBoxState
+	 * @return
+	 */
 	Qt::CheckState GetForceCheckBoxState();
 
+	/**
+	 * @brief UpdateInputChannelCurrentValue
+	 * @param in
+	 */
 	void UpdateInputChannelCurrentValue(ProcessImageIn *in);
+
+	/**
+	 * @brief UpdateOutputChannelCurrentValue
+	 * @param out
+	 */
 	void UpdateOutputChannelCurrentValue(ProcessImageOut *out);
 
 private slots:
+	/**
+	 * @brief on_check_stateChanged
+	 * @param arg1
+	 */
 	void on_check_stateChanged(int arg1);
 
+	/**
+	 * @brief on_force_stateChanged
+	 * @param arg1
+	 */
 	void on_force_stateChanged(int arg1);
 
 private:
@@ -47,3 +140,4 @@ private:
 };
 
 #endif // _UI_CHANNEL_H_
+
--- apps/demo_qt_advanced/plk_qt_gui/include/DialogOpenCdc.h
+++ apps/demo_qt_advanced/plk_qt_gui/include/DialogOpenCdc.h
@@ -1,28 +1,85 @@
+/**
+********************************************************************************
+\file   DialogOpenCdc.h
+
+\brief
+
+\author Ramakrishnan Periyakaruppan
+
+\copyright (c) 2014, Kalycito Infotech Private Limited
+					 All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+	* Redistributions of source code must retain the above copyright
+	  notice, this list of conditions and the following disclaimer.
+	* Redistributions in binary form must reproduce the above copyright
+	  notice, this list of conditions and the following disclaimer in the
+	  documentation and/or other materials provided with the distribution.
+	* Neither the name of the copyright holders nor the
+	  names of its contributors may be used to endorse or promote products
+	  derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL COPYRIGHT HOLDERS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
 #ifndef _UI_DIALOGOPENCDC_H_
 #define _UI_DIALOGOPENCDC_H_
 
+/*******************************************************************************
+* INCLUDES
+*******************************************************************************/
+
 #include "ui_DialogOpenCdc.h"
 
+/**
+ * @brief The DialogOpenCdc class
+ */
 class DialogOpenCdc : public QDialog
 {
 	Q_OBJECT
 
 public:
+	/**
+	 * @brief DialogOpenCdc
+	 * @param parent
+	 */
 	explicit DialogOpenCdc(QWidget *parent = 0);
 
 private slots:
+	/**
+	 * @brief on_browseCDC_clicked
+	 */
 	void on_browseCDC_clicked();
 
+	/**
+	 * @brief on_openCdcDialog_accepted
+	 */
 	void on_openCdcDialog_accepted();
 
+	/**
+	 * @brief on_openCdcDialog_rejected
+	 */
 	void on_openCdcDialog_rejected();
 
+	/**
+	 * @brief on_cdcPath_textEdited
+	 * @param arg1
+	 */
 	void on_cdcPath_textEdited(const QString &arg1);
 
 private:
 	Ui::DialogOpenCdc ui;
 	QString cdcPath;
-	// Q_DISABLE_COPY(DialogOpenCdc)
 };
 
 #endif // _UI_DIALOGOPENCDC_H_
