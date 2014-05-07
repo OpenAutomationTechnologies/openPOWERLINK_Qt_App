22ed670746ad93667654017f75beeeaf5a60b692
Ramakrishnan P <ramakrishanan_p@kalycito.com>
2014-05-07 23:41:01 +0530

    [TASK] Set force value even when focus out.
    Signed-off-by: Ramakrishnan P <ramakrishanan_p@kalycito.com>

--- apps/demo_qt_advanced/plk_qt_gui/src/ChannelUi.cpp
+++ apps/demo_qt_advanced/plk_qt_gui/src/ChannelUi.cpp
@@ -48,7 +48,8 @@ ChannelUi::ChannelUi(const Channel &channel, QWidget *parent) :
 	channel(channel),
 	lockValueTexbox(false),
 	input(NULL),
-	value(new LineEditUi())
+	value(new LineEditUi()),
+	valueBeforeLock("")
 {
 	ui.setupUi(this);
 
@@ -97,7 +98,6 @@ ChannelUi::~ChannelUi()
 	delete this->ui.check;
 	delete this->value;
 	delete this->ui.horizontalLayout;
-
 }
 
 void ChannelUi::UpdateSelectCheckBox(Qt::CheckState state)
@@ -197,7 +197,8 @@ void ChannelUi::ValueReturnPressed()
 	try
 	{
 		const QString forceValue = this->value->text();
-		if (!forceValue.isEmpty())
+		if (!forceValue.isEmpty()
+			&& (forceValue.compare(this->valueBeforeLock, Qt::CaseInsensitive) != 0))
 		{
 			const qlonglong forc = forceValue.toLongLong(0, 16);
 
@@ -216,12 +217,32 @@ void ChannelUi::ValueReturnPressed()
 
 void ChannelUi::LockCurrentValue()
 {
+	this->valueBeforeLock = this->value->text();
 	this->lockValueTexbox = true;
 	this->setStyleSheet("QLineEdit{background: yellow;}");
 }
 
 void ChannelUi::UnlockCurrentValue()
 {
+	try
+	{
+		const QString forceValue = this->value->text();
+		if (!forceValue.isEmpty()
+			&& (forceValue.compare(this->valueBeforeLock, Qt::CaseInsensitive) != 0))
+		{
+			const qlonglong forc = forceValue.toLongLong(0, 16);
+
+			this->input->SetRawValue(this->channel.GetName(),
+							(const void*) &forc,
+							this->channel.GetBitSize());
+		}
+	}
+	catch(const std::exception& ex)
+	{
+		// TODO Discuss about exposing the error to the user.
+		qDebug("An Exception has occured: %s", ex.what());
+	}
+
 	this->lockValueTexbox = false;
 	this->setStyleSheet("QLineEdit{background: white;}");
 }
