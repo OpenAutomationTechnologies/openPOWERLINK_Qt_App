fa212f1287db904b5c2e182b3ee3e36ddf7d0b02
Ramakrishnan P <ramakrishnan_p@kalycito.com>
2014-06-03 18:31:39 +0530

    [FIX] Fix compilation issues
     - Fix case sensitive QT include paths

--- apps/plk_qt_mn_gui/src/ChannelWidget.cpp
+++ apps/plk_qt_mn_gui/src/ChannelWidget.cpp
@@ -35,7 +35,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /*******************************************************************************
 * INCLUDES
 *******************************************************************************/
-#include <Qtcore/QDebug>
+#include <QtCore/QDebug>
 #include "ChannelWidget.h"
 #include "LineEditWidget.h"
 #include "user/processimage/ProcessImageIn.h"
